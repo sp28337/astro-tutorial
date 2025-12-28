f04bc51ebb24fbbcd170b11c96640c162c1318b7
Pavel Tarakanov <sp28337@yandex.ru>
2025-12-28 16:55:21 +0300

    chore(RSS): add an RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-tutorial-sp28337.netlify.app/",
   vite: {    
     plugins: [tailwindcss()],  
   },
